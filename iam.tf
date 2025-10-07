7419cc939c8650d342a2936c275398807c0bdf2f
Caul137 <146390141+Caul137@users.noreply.github.com>
2025-10-07 20:03:32 -0300

    first commit

---
+++ iam.tf
@@ -0,0 +1,57 @@
+
+resource "aws_iam_openid_connect_provider" "oid-git" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+
+   tags = {
+    IAC = true
+  }
+}
+
+resource "aws_iam_role" "app-runner-role" {
+  name = "app-runner-role"
+
+  assume_role_policy = jsonencode({
+    Version: "2012-10-17"
+    Statement: [{
+      Effect: "Allow",
+      Principal: {
+        Service: "build.apprunner.amazonaws.com"
+      },
+      Action: "sts:AssumeRole"
+    }
+   ]
+  })
+  managed_policy_arns = [
+    "arn:aws:iam:aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ]
+}
+
+
+
+resource "aws_iam_role" "test_role" {
+  name = "ecr_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    IAC = "true"
+  }
+}
